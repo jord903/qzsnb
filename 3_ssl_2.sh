5cc8d9cb9379dfe2f2538f6f24f03b851294df9c
mimihome-PC\mimihome <jord903@gmail.com>
2017-12-01 21:26:30 +0800

    remove sudo

--- 3_ssl_2.sh
+++ 3_ssl_2.sh
@@ -5,7 +5,7 @@ sudo cp /home/jord903/.acme.sh/$FQDN/ca.cer /usr/share/ca-certificates/ca.crt
 sudo dpkg-reconfigure ca-certificates
 
 zen-cli stop
-sudo cat <<EOF >> ~/.zen/zen.conf
+cat <<EOF >> ~/.zen/zen.conf
 tlscertpath=/home/jord903/.acme.sh/$FQDN/$FQDN.cer 
 tlskeypath=/home/jord903/.acme.sh/$FQDN/$FQDN.key
 EOF
